c645fa0250b4c52b78172dbe5248f628a0065553
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-03 23:53:17 -0600

    Completed Word Connector No testing or documentation yet.

--- word-connector.rb
+++ word-connector.rb
@@ -1,20 +1,38 @@
 require 'pry'
+require 'active_support'
+require 'active_support/core_ext/array/conversions'
+
 
 class WordConnector
   
-  def initialize
-    @blue_red_green = ["Blue", "Red", "Green"]
-    @blue_red = ["Blue", "Red"]
-  end
-  
-  def blue_red_green_merge_with_and
-    @blue_red_green[0..1].join(', ') + " and " + @blue_red_green[2]
+  def blue_red_green_merge_with_and(*words)
+    words.to_sentence
   end
 
-  def blue_red_merge_with_and
-    @blue_red[0] + " and " + @blue_red[1]
+  def blue_red_merge_with_and(*words)
+    words.to_sentence
   end
     
 end
+
+
+# ORIGINAL - NO ACTIVE SUPPORT
+# class WordConnector
+#
+#   def initialize
+#     @blue_red_green = ["Blue", "Red", "Green"]
+#     @blue_red = ["Blue", "Red"]
+#   end
+#
+#   def blue_red_green_merge_with_and
+#     @blue_red_green[0..1].join(', ') + " and " + @blue_red_green[2]
+#   end
+#
+#   def blue_red_merge_with_and
+#     @blue_red[0] + " and " + @blue_red[1]
+#   end
+#
+# end
+
 
 binding.pry
