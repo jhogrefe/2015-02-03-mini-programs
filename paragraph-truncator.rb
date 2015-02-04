6459cadb3b432e3733241299380e704bccb786f3
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-04 09:38:05 -0600

    Updated Paragraph Truncator with documentation.

--- paragraph-truncator.rb
+++ paragraph-truncator.rb
@@ -2,34 +2,39 @@ require 'pry'
 require 'active_support'
 require 'active_support/core_ext/string/filters'
 
+# Public: Paragraph Truncator class
+# Truncate a long string to "x" characters using Active
+# Support truncate method.
+#
+# Attributes:
+# None.
+#
+# Methods:
+# #truncate
 
-
-# Truncate long string to x characters using Truncate Active Support method
 class ParagraphTruncator
+
+  # Public: truncate method
+  # Shortens the character length of a string by the length 
+  # argument.
+  #
+  # Parameters:
+  # string - string, the text entered by the user of any length.
+  # length - the number of characters printed before the string is
+  #          truncated.
+  #
+  # Returns:
+  # String - the truncated string.
+  #
+  # State Changes:
+  # None.
   
-  def truncate(string, x)
-    string.truncate(x)
+  def truncate(string, length)
+    string.truncate(length)
   end
 
+# End of the class.    
 end
-
-
-
-# class ParagraphTruncator
-#
-#   attr_accessor :string, :length, :ending
-#
-#   def initialize(string, length, ending="...")
-#     @string = string
-#     @length = length
-#     @ending = ending
-#   end
-#
-#   def truncate
-#     @string.slice(0..@length) + "#{@ending}"
-#   end
-#
-# end
 
 
 binding.pry
