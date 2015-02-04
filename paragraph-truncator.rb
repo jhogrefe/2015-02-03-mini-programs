7f36afba8007eabbde925da45dc67d284f8190e4
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-04 00:06:04 -0600

    Completed Paragraph Truncator with Active Support. No testing or documentation.

--- paragraph-truncator.rb
+++ paragraph-truncator.rb
@@ -1,20 +1,35 @@
 require 'pry'
+require 'active_support'
+require 'active_support/core_ext/string/filters'
 
-# Truncate long string to x characters using Slice method
+
+
+# Truncate long string to x characters using Truncate Active Support method
 class ParagraphTruncator
   
-  attr_accessor :string, :length, :ending
-  
-  def initialize(string, length, ending="...")
-    @string = string
-    @length = length
-    @ending = ending
-  end
-  
-  def truncate
-    @string.slice(0..@length) + "#{@ending}"
+  def truncate(string, x)
+    string.truncate(x)
   end
 
 end
+
+
+
+# class ParagraphTruncator
+#
+#   attr_accessor :string, :length, :ending
+#
+#   def initialize(string, length, ending="...")
+#     @string = string
+#     @length = length
+#     @ending = ending
+#   end
+#
+#   def truncate
+#     @string.slice(0..@length) + "#{@ending}"
+#   end
+#
+# end
+
 
 binding.pry
